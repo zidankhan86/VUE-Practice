609b6b24612da5db912652878be1ca4a4112f88a
zidankhan86 <zidankhan718@gmail.com>
2024-01-24 19:52:27 +0600

    chore:

---
+++ es6.js
@@ -0,0 +1,9 @@
+const fname = "zidan"
+const lname = "khan"
+
+// const output ="His name is " + fname + " " + lname
+// console.log(output)
+
+const output2 =`his name is ${fname} ${lname}`
+
+console.log(output2)
